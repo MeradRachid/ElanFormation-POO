124c3a3680abf111bd7fc8aa7b39acbb056d1a10
MERAD Rachid <meradrpro@sfr.fr>
2023-03-03 14:02:56 +0100

    Mise en Place du PDO echo title & release_date

---
+++ Cinema/index.sql
@@ -0,0 +1,465 @@
+-- >> Requêtes SQL à faire :
+
+-- a) Information d'un film (film_id) : titre, année, durée au format(HH:MM) & réalisateur.
+SELECT 
+    m.movie_title AS titre, 
+    YEAR(m.release_date) AS annee, 
+    CONCAT(FLOOR(m.duration / 60), ':', LPAD(MOD(m.duration, 60), 2, '0')) AS duree, 
+    p.firstName AS prenom_realisateur, 
+    p.lastName AS nom_realisateur
+FROM 
+    movie m
+    JOIN person p ON m.director_id = p.person_id
+WHERE 
+    m.movie_id = [film_id];
+-- Remplacez <film_id> par l'ID du film souhaité.
+
+-- b) Liste des films dont la durée excède 2h15, classé par durée décroissante.
+SELECT movie_title, duration
+FROM movie
+WHERE duration > 135
+ORDER BY duration DESC;
+
+-- c) Liste des films d'un réalisateur, classé par ordre de sortie.
+SELECT m.movie_title, m.release_date
+FROM movie m
+INNER JOIN director d ON m.director_id = d.director_id
+WHERE d.director_id = [director_id]
+ORDER BY m.release_date;
+
+-- d) Nombre de films par genre (décroissant).
+SELECT genre_name, COUNT(*) AS num_movies
+FROM movie_genre mg
+INNER JOIN genre g ON mg.genre_id = g.genre_id
+GROUP BY mg.genre_id
+ORDER BY num_movies DESC;
+
+-- e) Nombre de films par réalisateur (décroissant).
+SELECT person.firstName, person.lastName, COUNT(movie.movie_id) as num_movies
+FROM director
+JOIN person ON director.person_id = person.person_id
+JOIN movie ON director.director_id = movie.director_id
+GROUP BY person.person_id
+ORDER BY num_movies DESC;
+
+-- f) Casting d'un film (film_id) : nom, prenom & genre.
+SELECT p.firstName, p.lastName, p.gender, mc2.character_name
+FROM person p
+JOIN actor a ON p.person_id = a.person_id
+JOIN movie_cast mc ON a.actor_id = mc.actor_id
+JOIN movie_character mc2 ON mc.character_id = mc2.character_id
+WHERE mc.movie_id = [film_id];
+-- Remplacez <film_id> par l'ID du film souhaité.
+
+-- g) Films tournés par un acteur(actor_id) avec le rôle et l'année de sortie (récent vers ancien).
+SELECT mc2.character_name, p.firstName, p.lastName, DATE_FORMAT(m.release_date, '%Y-%M-%D') as release_date
+FROM movie_cast mc
+JOIN person p ON mc.actor_id = p.person_id
+JOIN movie m ON mc.movie_id = m.movie_id
+JOIN movie_character mc2 ON mc.character_id = mc2.character_id
+WHERE mc.actor_id = [actor_id]
+ORDER BY m.release_date DESC;
+-- Remplacez <actor_id> par l'ID de l'acteur souhaité.
+
+-- h) Liste des personnes qui sont à la fois acteurs et réalisateurs.
+SELECT DISTINCT p.firstName, p.lastName
+FROM person p
+INNER JOIN director d ON p.person_id = d.person_id
+INNER JOIN actor a ON p.person_id = a.person_id;
+
+-- i) Liste des films qui ont moins de 10 ans.
+SELECT movie_title, release_date
+FROM movie
+WHERE release_date >= DATE_SUB(NOW(), INTERVAL 10 YEAR)
+ORDER BY release_date DESC;
+
+-- j): Parité Homme/Femme 
+SELECT p.gender, COUNT(*) AS count
+FROM actor a
+JOIN person p ON a.person_id = p.person_id
+GROUP BY p.gender;
+
+-- k) : Acteurs ayant 50 ans ou plus 
+SELECT p.firstName, p.lastName, YEAR(CURDATE()) - YEAR(p.birth_date) AS age
+FROM person p
+JOIN actor a ON p.person_id = a.actor_id
+WHERE YEAR(CURDATE()) - YEAR(p.birth_date) >= 50
+ORDER BY age DESC;
+-- On joint la table person avec la table actor pour récupérer seulement les acteurs.
+-- On utilise la fonction YEAR() pour extraire l'année de la date de naissance de chaque acteur,
+-- On soustrait cette année de l'année actuelle (obtenue avec la fonction CURDATE()), pour calculer l'âge de chaque acteur.
+-- On filtre sur les acteurs de 50 ans ou plus en utilisant une condition dans la clause WHERE.
+-- On trie les résultats par ordre décroissant d'âge avec la clause ORDER BY.
+
+-- l) : Acteurs ayant joués dans 3 films ou plus
+SELECT p.firstName, p.lastName, COUNT(DISTINCT mc.movie_id) AS num_movies
+FROM person p
+JOIN movie_cast mc ON p.person_id = mc.actor_id
+GROUP BY p.person_id
+HAVING num_movies >= 3
+ORDER BY p.lastName, p.firstName;
+-- On fait une jointure entre les tables person et movie_cast sur la colonne person_id qui correspond à l'ID des acteurs.
+-- On utilise la fonction COUNT(DISTINCT mc.movie_id) pour compter le nombre de films distincts dans lesquels chaque acteur a joué. 
+-- On utilise DISTINCT pour s'assurer qu'on ne compte pas plusieurs fois le même film si un acteur a joué plusieurs personnages dans ce film.
+-- On regroupe les résultats par person_id (l'ID de l'acteur) en utilisant la clause GROUP BY.
+-- On utilise la clause HAVING pour filtrer les acteurs qui ont joué dans moins de 3 films.
+-- Enfin, on trie les résultats par nom de famille et prénom en utilisant la clause ORDER BY.
+
+
+
+-- >> création Tables BDD cinema : 
+CREATE TABLE Genre 
+(
+    genre_id INT PRIMARY KEY AUTO_INCREMENT,
+    genre_name VARCHAR(90) NOT NULL
+);
+
+CREATE TABLE Person 
+(
+    person_id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(90) NOT NULL,
+    lastName VARCHAR(90) NOT NULL,
+    birth_date DATETIME NOT NULL,
+    gender VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Movie_character 
+(
+    character_id INT PRIMARY KEY AUTO_INCREMENT,
+    character_name VARCHAR(90) NOT NULL
+);
+
+CREATE TABLE Actor 
+(
+    actor_id INT PRIMARY KEY AUTO_INCREMENT,
+    person_id INT NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES Person(person_id)
+);
+
+CREATE TABLE Director 
+(
+    director_id INT PRIMARY KEY AUTO_INCREMENT,
+    person_id INT NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES Person(person_id)
+);
+
+CREATE TABLE Movie 
+(
+    movie_id INT PRIMARY KEY AUTO_INCREMENT,
+    movie_title VARCHAR(255) NOT NULL,
+    release_date DATETIME NOT NULL,
+    duration TIME NOT NULL,
+    synopsis TEXT NOT NULL,
+    rating FLOAT(2) NOT NULL,
+    poster VARCHAR(255),
+    director_id INT NOT NULL,
+    FOREIGN KEY (director_id) REFERENCES Director(director_id)
+);
+
+CREATE TABLE Movie_genre 
+(
+    movie_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    PRIMARY KEY (movie_id, genre_id),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+    FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
+);
+
+CREATE TABLE Movie_cast 
+(
+    movie_id INT NOT NULL,
+    actor_id INT NOT NULL,
+    character_id INT NOT NULL,
+    PRIMARY KEY (movie_id, actor_id, character_id),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+    FOREIGN KEY (actor_id) REFERENCES Actor(actor_id),
+    FOREIGN KEY (character_id) REFERENCES Movie_character(character_id)
+);
+
+-- >> insertion Genre Cinématographique :  
+INSERT INTO genre (genre_id, genre_name) VALUES
+    (1, 'Action'),
+    (2, 'Adventure'),
+    (3, 'Fantasy');
+
+
+-- >> insertion Films : 
+INSERT INTO movie (movie_id, movie_title, release_date, duration, synopsis, rating, poster, director_id) VALUES
+(1, 'The Lord of the Rings: The Fellowship of the Ring', '2001-12-19', '178', 'A young hobbit, Frodo, who has found the One Ring that belongs to the Dark Lord Sauron, begins his journey with eight companions to Mount Doom, the only place where it can be destroyed.', 4.8, NULL, 1),
+(2, 'The Lord of the Rings: The Two Towers', '2002-12-18', '179', 'While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron''s new ally, Saruman, and his hordes of Isengard.', 4.9, NULL, 1),
+(3, 'The Lord of the Rings: The Return of the King', '2003-12-17', '201', 'Gandalf and Aragorn lead the World of Men against Sauron''s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.', 5, NULL, 1),
+(4, 'The Hobbit: An Unexpected Journey', '2012-12-13', '169', 'A reluctant hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it from the dragon Smaug.', 3.7, NULL, 2),
+(5, 'The Hobbit: The Desolation of Smaug', '2013-12-13', '161', 'The dwarves, along with Bilbo Baggins and Gandalf the Grey, continue their quest to reclaim Erebor, their homeland, from Smaug. Bilbo Baggins is in possession of a mysterious and magical ring.', 4.1, NULL, 2),
+(6, 'The Hobbit: The Battle of the Five Armies', '2014-12-17', '144', 'Bilbo and company are forced to engage in a war against an array of combatants and keep the Lonely Mountain from falling into the hands of a rising darkness.', 4, NULL, 2);
+
+-- >> insertion Personnages : 
+INSERT INTO movie_character (character_id, character_name) VALUES
+(1, 'Frodo Baggins'),
+(2, 'Gandalf'),
+(3, 'Aragorn'),
+(4, 'Legolas'),
+(5, 'Gimli'),
+(6, 'Samwise Gamgee'),
+(7, 'Peregrin Took'),
+(8, 'Meriadoc Brandybuck'),
+(9, 'Saruman'),
+(10, 'Arwen Undómiel'),
+(11, 'Elrond'),
+(12, 'Galadriel'),
+(13, 'Sauron'),
+(14, 'Éowyn'),
+(15, 'Éomer'),
+(16, 'Faramir'),
+(17, 'Denethor'),
+(18, 'Gollum'),
+(19, 'Bilbo Baggins'),
+(20, 'Thorin Oakenshield'),
+(21, 'Balin'),
+(22, 'Dwalin'),
+(23, 'Kíli'),
+(24, 'Fili'),
+(25, 'Bofur'),
+(26, 'Bifur'),
+(27, 'Bombur'),
+(28, 'Óin'),
+(29, 'Glóin'),
+(30, 'Dori'),
+(31, 'Nori'),
+(32, 'Ori'),
+(33, 'Radagast'),
+(34, 'Tauriel'),
+(35, 'Bowman'),
+(36, 'Beorn'),
+(37, 'Smaug The Dragon'),
+(38, 'Cameo');
+
+-- >> insertion Personnes : 
+INSERT INTO person (person_id, firstName, lastName, birth_date, gender) VALUES
+(1, 'Elijah', 'Wood', '1981-01-28', 'M'),
+(2, 'Ian', 'McKellen', '1939-05-25', 'M'),
+(3, 'Viggo', 'Mortensen', '1958-10-20', 'M'),
+(4, 'Orlando', 'Bloom', '1977-01-13', 'M'),
+(5, 'John', 'Rhys-Davies', '1944-05-05', 'M'),
+(6, 'Sean', 'Astin', '1971-02-25', 'M'),
+(7, 'Billy', 'Boyd', '1968-08-28', 'M'),
+(8, 'Dominic', 'Monaghan', '1976-12-08', 'M'),
+(9, 'Christopher', 'Lee', '1922-05-27', 'M'),
+(10, 'Liv', 'Tyler', '1977-07-01', 'F'),
+(11, 'Hugo', 'Weaving', '1960-04-04', 'M'),
+(12, 'Cate', 'Blanchett', '1969-05-14', 'F'),
+(13, 'Sala', 'Baker', '1976-09-22', 'M'),
+(14, 'Miranda', 'Otto', '1967-12-15', 'F'),
+(15, 'Karl', 'Urban', '1972-06-07', 'M'),
+(16, 'David', 'Wenham', '1965-09-21', 'M'),
+(17, 'John', 'Noble', '1948-08-20', 'M'),
+(18, 'Andy', 'Serkis', '1964-04-20', 'M'),
+(19, 'Ian', 'Holm', '1931-09-12', 'M'),
+(20, 'Martin', 'Freeman', '1971-09-08', 'M'),
+(21, 'Richard', 'Armitage', '1971-08-22', 'M'), 
+(22, 'Benedict', 'Cumberbatch', '1976-07-19', 'M'),
+(23, 'Ken', 'Stott', '1954-10-19', 'M'),
+(24, 'Graham', 'McTavish', '1961-01-04', 'M'),
+(25, 'Aidan', 'Turner', '1983-06-19', 'M'),
+(26, 'Dean', 'O.Gorman', '1976-12-01', 'M'),
+(27, 'James', 'Nesbitt', '1965-01-15', 'M'),
+(28, 'William', 'Kircher', '1958-05-23', 'M'),
+(29, 'Stephen', 'Hunter', '1968-10-26', 'M'),
+(30, 'John', 'Callen', '1946-11-04', 'M'),
+(31, 'Peter', 'Hambleton', '1976-07-19', 'M'),
+(32, 'Mark', 'Hadlow', '1976-07-19', 'M'),
+(33, 'Jed', 'Brophy', '1963-10-29', 'M'),
+(34, 'Adam', 'Brown', '1980-05-29', 'M'),
+(35, 'Sylvester', 'Mc Coy', '1943-08-20', 'M'),
+(36, 'Evangeline', 'Lilly', '1979-08-03', 'F'),
+(37, 'Luke', 'Evans', '1979-04-15', 'M'),
+(38, 'Mikael', 'Persbrandt', '1963-09-25', 'M'),
+(39, 'Peter', 'Jackson', '1961-10-31', 'M');
+
+-- >> insertion Acteurs : 
+INSERT INTO Actor (actor_id, person_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(11, 11),
+(12, 12),
+(13, 13),
+(14, 14),
+(15, 15),
+(16, 16),
+(17, 17),
+(18, 18),
+(19, 19),
+(20, 20),
+(21, 21),
+(22, 22),
+(23, 23),
+(24, 24),
+(25, 25),
+(26, 26),
+(27, 27),
+(28, 28),
+(29, 29),
+(30, 30),
+(31, 31),
+(32, 32),
+(33, 33),
+(34, 34),
+(35, 35),
+(36, 36),
+(37, 37),
+(38, 38),
+(39, 39);
+-- etc.
+
+-- >> insertion Realisateur : 
+INSERT INTO director (id_director, person_id) VALUES
+(1, 39),
+(2, 18);
+
+-- >> insertion Association du genre à un film : 
+-- Par exemple, le premier enregistrement indique que le film avec l'id 1 est associé au genre avec l'id 2, et au genre avec l'id 3.
+INSERT INTO movie_genre (movie_id, genre_id) 
+VALUES 
+(1, 2),
+(1, 3),
+(2, 1),
+(3, 2),
+(3, 3);
+
+-- >> insertion Association d'un acteur à son personnage dans un film : 
+-- Par exemple, le premier enregistrement indique que l'acteur avec l'id 1 joue le personnage avec l'id 1 dans le film avec l'id 1, et que l'acteur avec l'id 2 joue le personnage avec l'id 2 dans le même film.
+INSERT INTO movie_cast (movie_id, actor_id, character_id)
+VALUES 
+(1, 1, 1),
+(1, 2, 2),
+(1, 3, 3),
+(1, 4, 4),
+(1, 5, 5),
+(1, 6, 6),
+(1, 7, 7),
+(1, 8, 8),
+(1, 9, 9),
+(1, 10, 10),
+(1, 11, 11),
+(1, 12, 12),
+(1, 13, 13),
+(1, 18, 18),
+(2, 1, 1),
+(2, 2, 2),
+(2, 3, 3),
+(2, 4, 4),
+(2, 5, 5),
+(2, 6, 6),
+(2, 7, 7),
+(2, 8, 8),
+(2, 9, 9),
+(2, 10, 10),
+(2, 11, 11),
+(2, 12, 12),
+(2, 13, 13),
+(3, 18, 18),
+(3, 1, 1),
+(3, 2, 2),
+(3, 3, 3),
+(3, 4, 4),
+(3, 5, 5),
+(3, 6, 6),
+(3, 7, 7),
+(3, 8, 8),
+(3, 9, 9),
+(3, 10, 10),
+(3, 11, 11),
+(3, 12, 12),
+(3, 13, 13),
+(3, 18, 18),
+(2, 14, 14),
+(2, 15, 15),
+(2, 16, 16),
+(2, 17, 17),
+(3, 14, 14),
+(3, 15, 15),
+(3, 16, 16),
+(3, 17, 17),
+(1, 39, 38),
+(3, 39, 38),
+(1, 19, 19),
+(3, 19, 19),
+(4, 20, 19),
+(5, 20, 19),
+(6, 20, 19),
+(6, 19, 19),
+(4, 21, 20),
+(5, 21, 20),
+(6, 21, 20),
+(4, 22, 37),
+(5, 22, 37),
+(6, 22, 37),
+(4, 39, 38),
+(5, 39, 38),
+(4, 23, 21),
+(5, 23, 21),
+(6, 23, 21),
+(4, 24, 22),
+(5, 24, 22),
+(6, 24, 22),
+(4, 25, 23),
+(5, 25, 23),
+(6, 25, 23),
+(4, 26, 24),
+(5, 26, 24),
+(6, 26, 24),
+(4, 27, 25),
+(5, 27, 25),
+(6, 27, 25),
+(4, 28, 26),
+(5, 28, 26),
+(6, 28, 26),
+(4, 29, 27),
+(5, 29, 27),
+(6, 29, 27),
+(4, 30, 28),
+(5, 30, 28),
+(6, 30, 28),
+(4, 31, 29),
+(5, 31, 29),
+(6, 31, 29),
+(4, 32, 30),
+(5, 32, 30),
+(6, 32, 30),
+(4, 33, 31),
+(5, 33, 31),
+(6, 33, 31),
+(4, 34, 32),
+(5, 34, 32),
+(6, 34, 32),
+(4, 35, 33),
+(5, 35, 33),
+(6, 35, 33),
+(4, 36, 34),
+(5, 36, 34),
+(6, 36, 34),
+(4, 37, 35),
+(5, 37, 35),
+(6, 37, 35),
+(4, 38, 36),
+(5, 38, 36),
+(6, 38, 36);
+
+
+
+-- https://fr.wikipedia.org/wiki/Le_Seigneur_des_anneaux_(s%C3%A9rie_de_films)
+
+-- https://fr.wikipedia.org/wiki/Liste_des_films_de_James_Bond
+
+-- https://fr.wikipedia.org/wiki/Indiana_Jones_(franchise)
+
+-- https://fr.wikipedia.org/wiki/Star_Wars_au_cin%C3%A9ma
+
+-- https://fr.wikipedia.org/wiki/La_Famille_foldingue
---
+++ Cinema/pdo.php
@@ -0,0 +1,92 @@
+<!-- 
+    PDO ("PHP Data Objects") : Extension fournie avec PHP, mais elle doit être activé manuellement dans certains cas. 
+
+    try et catch sont des blocs de code utilisés en programmation pour gérer les erreurs et exceptions qui peuvent se produire pendant l'exécution d'un programme.
+
+    Le bloc try contient le code qui peut potentiellement générer une exception. 
+    Si une exception est effectivement générée, le programme quitte le bloc try et passe immédiatement au bloc catch.
+
+    Le bloc catch contient le code qui est exécuté en cas d'exception, et permet de gérer cette exception. 
+    Le bloc catch peut recevoir l'objet exception généré dans le bloc try, ce qui permet de récupérer des informations sur l'erreur, telles que son message et sa cause.
+
+    Voici un exemple de code qui utilise try et catch : 
+
+    try 
+    {
+        // code qui peut générer une exception
+    }
+    
+    catch (Exception $e) 
+    {
+        // code qui gère l'exception
+    }
+    
+    L'utilisation de try et catch est recommandée dans les programmes où des erreurs peuvent se produire.
+    Car cela permet de gérer ces erreurs et ainsi empêcher le programme de se bloquer ou de planter.
+
+-->
+
+<?php
+
+try 
+{   // On définit à présent les informations de connexion à la base de données avec les variables   $host, $dbname, $username et $password.
+    $pdo = new PDO("mysql:host=localhost;dbname=cinema;charset=utf8", "root", "");
+    // Ensuite, on utilise la classe PDO pour se connecter à la base de données en utilisant les informations de connexion définies précédemment.
+}
+catch (PDOException $e) 
+{
+        echo "Erreur de connexion : " . $e->getMessage();
+    } 	// En cas d'erreur, on affiche un message et on arrête tout
+    
+    
+    // Si tout va bien, on peut continuer
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // On fait appel à la méthode setAttribute() pour configurer certaines options du gestionnaire de requêtes PDO (PDOStatement).    
+
+    // Le premier appel, $stmtCinema->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);, configure le mode d'erreur de PDO. 
+    // On configure donc PDO pour qu'il lance une exception de type PDOException en cas d'erreur SQL. 
+    // Cela permet de capturer les erreurs SQL et de les gérer de manière plus précise.
+    // Par exemple en affichant un message d'erreur approprié à l'utilisateur.
+
+    // Le second appel, $stmtCinema->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);, désactive la fonctionnalité d'émulation de requêtes préparées de PDO. 
+    // Par défaut, PDO émule les requêtes préparées côté client, ce qui peut potentiellement entraîner des failles de sécurité si les données entrantes ne sont pas correctement filtrées. 
+    // En désactivant cette émulation, on s'assure que les requêtes préparées sont bien utilisées côté serveur, ce qui garantit une meilleure sécurité.
+    // C'est pour cela qu'on utilise la méthode prepare().
+    // Car c'est ce qui nous permet d'optimiser l'exécution de la requête et de prévenir les attaques par injection SQL.
+    
+    // On définit une requête SQL qui sélectionne toutes les colonnes de la table movie en utilisant la chaîne de caractères 'SELECT * FROM movie'.
+    $stmtCinema = $pdo->query('SELECT movie_title, DATE_FORMAT(release_date, "%Y-%M-%D") AS release_date FROM movie');
+    // On récupère donc tout le contenu de la table movie : 
+
+    // La méthode query() est une méthode de l'objet PDO qui permet d'exécuter une requête SQL qui ne requiert pas de préparation préalable. La méthode prend en paramètre la requête SQL à exécuter et renvoie un objet PDOStatement qui peut être utilisé pour récupérer les résultats de la requête.
+    // La méthode query() peut être utilisée lorsque la requête SQL est statique et ne nécessite pas de lier des valeurs de paramètres dynamiquement. 
+    // Elle est utile pour les requêtes SELECT simples, mais elle ne permet pas de prévenir les attaques par injection SQL car les paramètres ne sont pas liés dynamiquement. 
+
+    // Par conséquent, il est recommandé d'utiliser la méthode prepare() pour toutes les requêtes qui incluent des variables ou des paramètres liés dynamiquement.
+    // Comme ceci : 
+    // $sqlQuery = 'SELECT * FROM movie';
+    // $cinemaStatement = $pdo->prepare($sqlQuery);
+    // $cinemaStatement->execute();
+    // $movie = $cinemaStatement->fetchAll();
+
+    // On utilise la méthode execute() de l'objet PDOStatement pour exécuter la déclaration préparée et obtenir un objet PDOStatement qui représente les résultats de la requête.
+    // La méthode fetchAll() est appelée sur l'objet $statement après l'exécution de la requête avec la méthode execute().
+    // On utilise la méthode fetchAll() de l'objet PDOStatement pour récupérer tous les résultats de la requête sous forme d'un tableau associatif. 
+    // Chaque élément du tableau représente une ligne de la table movie, avec les clés du tableau correspondant aux noms des colonnes de la table.    
+
+    $movies = $stmtCinema->fetchAll();
+    // // Le nom de variable $stmt est couramment utilisé pour représenter un objet de requête préparée (PDOStatement en PHP). 
+    // // Cette convention de nommage est assez répandue dans la communauté PHP pour représenter un objet de requête préparée qui sera utilisé pour exécuter une requête SQL.
+    // // Cela permet de distinguer clairement la requête SQL de la variable qui la stocke.
+    
+    echo "<h3> Movie Table : </h3>";
+
+    foreach ($movies as $movie) 
+    {
+        echo '<p>'.$movie['movie_title']." - ".$movie['release_date'].'<br></p>';
+    }
+    // Enfin, on parcourt le tableau de résultats en utilisant une boucle foreach qui permet d'afficher les informations de chaque film, comme le titre, l'année de sortie, la durée, etc. 
+    
+    ?>
