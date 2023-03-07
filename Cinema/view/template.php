8ae1b894976c729ff71ce121ce35473581052525
MERAD Rachid <meradrpro@sfr.fr>
2023-03-07 11:57:47 +0100

    Ajout de la navbar et des menus deroulants

--- Cinema/view/template.php
+++ Cinema/view/template.php
@@ -15,21 +15,75 @@ On exploitera ce qu'on appelle "la temporisation de sortie" en PHP -->
         <link rel="stylesheet" href="public/css/style.css"/> 
     </head>
     <body>
-        <?= $contenu ?>
-        <!-- /**
-         * On commence et on termine la vue par "ob_start()" et "ob_get_clean()"
-         * On va donc "aspirer" tout ce qui se trouve entre ces 2 fonctions (temporisation de sortie)
-         * Avec pour but de stocker le contenu dans une variable $contenu
-         * 
-         * Le require de fin permet d'injecter le contenu dans le template "squelette" template.php
-         * 
-         * Du coup dans notre "template.php" on aura des variables 
-         * Qui vont accueillir les éléments provenant des différentes vues
-         * Au final, DANS CHAQUE VUE, il faudra TOUJOURS : 
-         * Donner une valeur à $titre, $contenu et $titre_secondaire
-         *  */  -->
+        <div id="bodyWrap" class="container">
+            <nav class="d-flex align-items-center navbar navbar-expand-lg bg-primary p-1 rounded" data-bs-theme="dark">
+                <a href="index.php">
+                    <img src="public/img/RaiKoh.png" alt="Logo" width="43" height="43" class="d-inline-block align-text-top rounded">
+                </a>
+                <a class="navbar-brand ps-3" href="index.php"><strong> Middle-Earth Cinema </strong></a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Genres
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Aventure</a></li>
+                            <li><a class="dropdown-item" href="#">Fantasy</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Show All</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Films 
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Lord of Rings</a></li>
+                            <li><a class="dropdown-item" href="#">The Hobbit</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Show All</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Characters
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Lord of Rings</a></li>
+                            <li><a class="dropdown-item" href="#">The Hobbit</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Show All</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Directors
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Peter Jackson</a></li>
+                            <li><a class="dropdown-item" href="#">Andy Serkis</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Show All</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <?= $contenu ?>
+            <!-- /**
+            * On commence et on termine la vue par "ob_start()" et "ob_get_clean()"
+            * On va donc "aspirer" tout ce qui se trouve entre ces 2 fonctions (temporisation de sortie)
+            * Avec pour but de stocker le contenu dans une variable $contenu
+            * 
+            * Le require de fin permet d'injecter le contenu dans le template "squelette" template.php
+            * 
+            * Du coup dans notre "template.php" on aura des variables 
+            * Qui vont accueillir les éléments provenant des différentes vues
+            * Au final, DANS CHAQUE VUE, il faudra TOUJOURS : 
+            * Donner une valeur à $titre, $contenu et $titre_secondaire
+            *  */  -->
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
+        </div>
     </body>
 </html>
