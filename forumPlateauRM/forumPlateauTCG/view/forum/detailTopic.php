c4e1ba878bb4314d522761dc6ca1edfb5b1642db
MERAD Rachid <meradrpro@sfr.fr>
2023-04-04 20:13:51 +0200

    AddPost effectif

--- forumPlateauRM/forumPlateauTCG/view/forum/detailTopic.php
+++ forumPlateauRM/forumPlateauTCG/view/forum/detailTopic.php
@@ -12,20 +12,19 @@
 <h1>~ Bienvenue ~</h1>
 <h2 class="text-center"> Messages du Topic <?=$topic->getTopicTitle()?> </h2>
 
-
-<form action="index.php?ctrl=forum&action=like&id=<?=$topic->getId()?>" method="post">
-    <button type="submit" class="rounded p-1 border-info">Liked <?= $like ? "(" . $like . ")" : "" ?> </button>
-</form>
-
+<div class="d-flex justify-content-start align-items-center">
+    <form action="index.php?ctrl=forum&action=like&id=<?=$topic->getId()?>" method="post">
+        <button type="submit" class="rounded p-1 m-3 border-info">Liked <?= $like ? "(" . $like . ")" : "" ?> </button>
+    </form>
 
 <?php
     if (!empty($posts)) 
     {
-        echo '<label class="label-info text-center alert alert-warning" role="alert"> No post found here. Be the first to create one : </label> <br>
-              <form action="index.php?ctrl=forum&action=postForm" method="post" enctype="multipart/form-data">
+        echo '<form action="index.php?ctrl=forum&action=postForm" method="post" enctype="multipart/form-data">
                   <input type="hidden" name="topic_id" value="'.$topic->getId().'">
-                  <button type="submit" name="'.$topic->getId().'" class="rounded p-1 border-success" style="max-width: 18rem;">Create a New Message</button>
-              </form>';
+                  <button type="submit" name="'.$topic->getId().'" class="rounded p-1 m-3 border-success" style="max-width: 18rem;">Create a New Message</button>
+              </form>
+</div>';
         
         foreach($posts as $post) 
         {
@@ -61,40 +60,9 @@
         echo '<label class="label-info text-center alert alert-warning" role="alert"> No post found here. Be the first to create one : </label> <br>
                 <form action="index.php?ctrl=forum&action=postForm" method="post" enctype="multipart/form-data">
                     <input type="hidden" name="topic_id" value="'.$topic->getId().'">
-                    <button type="submit" name="'.$topic->getId().'" class="rounded p-1 border-success" style="max-width: 18rem;">Create a New Message</button>
+                    <button type="submit" name="'.$topic->getId().'" class="rounded p-1 m-3 border-success" style="max-width: 18rem;">Create a New Message</button>
                 </form>';
 
     }
 ?>
 
-<?php
-    foreach($posts as $post)
-    {
-?>
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                <th> 
-
-                    <?=$post->getCreationDate()?>
-
-                </th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr>
-                    <td>
-                        <?=$post->getMessage()?>   
-                    </td>
-                </tr>                
-                <tr>
-                    <td> 
-                        <em> <a href="index.php?ctrl=forum&action=detailUser&id=<?=$topic->getUser()->getId()?>" class="text-decoration-none"> By : <?= $topic->getUser()->getUserName() ?> </a>, in <?= $topic->getTopicTitle() ?> </em>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-<?php        
-
-    }
-?>
